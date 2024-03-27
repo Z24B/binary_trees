8fe1ebdb33a2c8e185fd509b5992334cca8c0774
Z24B <zivha.beharie@gmail.com>
2024-03-27 05:25:29 -0700

    Task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: The size of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t size_left = binary_tree_size(tree->left);
+	size_t size_right = binary_tree_size(tree->right);
+
+	return (size_left + size_right + 1);
+}
