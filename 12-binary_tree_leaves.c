1aed0a75f2ca02b205748db3714a2a09fbf09d7f
Z24B <zivha.beharie@gmail.com>
2024-03-27 05:36:55 -0700

    Task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,14 +8,12 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
+	if (!tree->left && !tree->right)
 		return (1);
 
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
-
-	return (left_leaves + right_leaves);
+	return (binary_tree_leaves(tree->left) +
+		binary_tree_leaves(tree->right));
 }
